06c692b2785c24ad47c99e39f83e738fef518f3b
Frankie WANG <qiuw@microsoft.com>
2019-06-29 18:10:21 +0800

    updated toString method

--- src/org/fw/model/User.java
+++ src/org/fw/model/User.java
@@ -23,7 +23,9 @@ public class User {
 
     @Override
     public String toString() {
-        return "User [name=" + name + "]";
+        return "User [name=" + name + ", age=" + age + "]";
     }
+
+ 
  
 }
